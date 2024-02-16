a3bae51330f3fa510da0fa2a78da38e61278ffe8
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-16 09:14:53 +0300

    Добавьте файлы проекта.

---
+++ FP_DZ_15_02_Task_2.cpp
@@ -0,0 +1,31 @@
+﻿// Написать программу, которая выводит одномерный массив в обратном порядке.
+
+
+#include <iostream>
+#include <windows.h>
+#include <random>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int sizeArr = 50;
+    int Arr[sizeArr];
+    random_device Random;
+
+    for (int i = 0; i < sizeArr; i++)
+    {
+        Arr[i] = i;        
+    }
+
+    cout << "Массив в обратном порядке : " << endl;
+
+
+    for (int i = sizeArr - 1; i > 0; i--)
+    {
+        cout << " " << Arr[i];
+    }    
+}
+
